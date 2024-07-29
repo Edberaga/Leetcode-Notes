72add15a9f475b61c269b44bf4b315f82a565ed0
edberaga <edbetjonnathan@gmail.com>
2024-07-29 17:01:33 +0700

    Intersectioction of two arrays II solution

---
+++ Easy/350. Intersection of Two Arrays II/main.js
@@ -0,0 +1,7 @@
+var intersect = function(arr1, arr2) {
+    return arr1.filter(value => arr2.includes(value));
+};
+
+console.log(intersect([1,2,2,1], [2,2])); //[2, 2]
+console.log(intersect([1,2,2,1], [2])); // [2]
+console.log(intersect([4,9,5], [9,4,9,8,4])); //[4, 9] or [9, 4]
