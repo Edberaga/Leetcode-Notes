3fcd3886feeb7a4e945c36bb9139ae0c9e426e7b
edberaga <edbetjonnathan@gmail.com>
2024-07-29 17:02:33 +0700

    Intersection of two arrays II solution

--- Easy/350. Intersection of Two Arrays II/main.js
+++ Easy/350. Intersection of Two Arrays II/main.js
@@ -1,6 +1,28 @@
-var intersect = function(arr1, arr2) {
-    return arr1.filter(value => arr2.includes(value));
-};
+function intersect(arr1, arr2) {
+    const map1 = new Map();
+    const map2 = new Map();
+
+    for (const num of arr1) {
+        map1.set(num, (map1.get(num) || 0) + 1);
+    }
+
+    for (const num of arr2) {
+        map2.set(num, (map2.get(num) || 0) + 1);
+    }
+
+    const result = [];
+    for (const [num, count1] of map1) {
+        if (map2.has(num)) {
+            const count2 = map2.get(num);
+            const minCount = Math.min(count1, count2);
+            for (let i = 0; i < minCount; i++) {
+                result.push(num);
+            }
+        }
+    }
+
+    return result;
+}
 
 console.log(intersect([1,2,2,1], [2,2])); //[2, 2]
 console.log(intersect([1,2,2,1], [2])); // [2]
